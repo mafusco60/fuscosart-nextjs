b400851b9841d0abbeceab605156c8fb8d8de11f
Mary Anne Fusco <fuscomaryanne@gmail.com>
2024-05-16 23:26:45 -0400

    layout homepage

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+	title: "Fusco's Art Gallery",
+	description: 'Find Your Favorite Artwork',
+	keywords:
+		'art, gallery, artwork, painting, drawings, prints, posters, canvas, wall art, home decor, interior design, art lovers, art collectors, art enthusiasts, art buyers, art sellers, art dealers, art investors, art critics, art historians, art students, art teachers, art professors, art curators, art directors, art consultants, art advisors, art appraisers, art conservators, art restorers',
+};
+
+const MainLayout = ({ children }) => {
+	return (
+		<html lang='en'>
+			<body>
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+};
+
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+	title: "Fusco's Art Gallery | Home",
+	description: 'Find Your Favorite Artwork',
+	keywords:
+		'art, gallery, artwork, painting, drawings, prints, posters, canvas, wall art, home decor, interior design, art lovers, art collectors, art enthusiasts, art buyers, art sellers, art dealers, art investors, art critics, art historians, art students, art teachers, art professors, art curators, art directors, art consultants, art advisors, art appraisers, art conservators, art restorers',
+};
+
+const HomePage = () => {
+	return <div>HomePage</div>;
+};
+
+export default HomePage;
