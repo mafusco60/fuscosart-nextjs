928192f724ca489db3a1a7787cef85cdffb44359
Mary Anne Fusco <fuscomaryanne@gmail.com>
2024-05-25 20:40:38 -0400

    Sign In Button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import logo from '@/assets/images/logo.png';
 import profileDefault from '@/assets/images/profile.png';
@@ -13,8 +13,16 @@ const Navbar = () => {
 	const { data: session } = useSession(); 
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 	const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+	const [providers, setProviders] = useState(null);
 	const pathname = usePathname();
-	const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+	useEffect(() => {
+		const setAuthProviders = async () => {
+			const res = await getProviders();
+			setProviders(res);
+		}
+		setAuthProviders();
+	}, []);
 
 	return (
 		<nav className='bg-rose-950 border-b border-red-950'>
@@ -25,7 +33,7 @@ const Navbar = () => {
 						<button
 							type='button'
 							id='mobile-dropdown-button'
-							className='relative inline-flex items-center justify-center rounded-md p-2 text-rose-600 hover:bg-rose-950 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white md:hidden'
+							className='relative inline-flex items-center justify-center rounded-md p-2 text-rose-900 hover:bg-rose-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white md:hidden'
 							aria-controls='mobile-menu'
 							aria-expanded='false'
 							onClick={() => setIsMobileMenuOpen((prev) => !prev)}
@@ -64,7 +72,7 @@ const Navbar = () => {
 									href='/'
 									className={`${
 										pathname === '/' ? 'text-rose-600' : 'text-white'
-									} hover:bg-rose-700 hover:text-white rounded-md px-3 py-2`}
+									} hover:bg-rose-900 hover:text-white rounded-md px-3 py-2`}
 								>
 									Home
 								</Link>
@@ -91,15 +99,23 @@ const Navbar = () => {
 					</div>
 					{/* <!-- Right Side Menu (Logged Out) --> */}
 					{!session && (
-						<div className='hidden md:block md:ml-6'>
-							<div className='flex items-center'>
-								<button className='flex items-center text-white bg-rose-600 hover:bg-rose-950 hover:text-white rounded-md px-3 py-2'>
-									<FaGoogle className='fa-brands fa-google text-white mr-2'></FaGoogle>
-									<span>Login or Register</span>
-								</button>
-							</div>
-						</div>
-					)}
+            <div className='hidden md:block md:ml-6'>
+              <div className='flex items-center'>
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      onClick={() => signIn(provider.id)}
+                      key={index}
+                      className='flex items-center text-white bg--700 bg-rose-900 hover:bg-rose-800 hover:text-white rounded-md px-3 py-2'
+                    >
+                      <FaGoogle className='text-white mr-2' />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </div>
+            </div>
+          )}
+					
 					{/* <!-- Right Side Menu (Logged In) --> */}{' '}
 					{session && (
 						<div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
@@ -215,7 +231,7 @@ const Navbar = () => {
 						>
 							Artworks
 						</Link>
-						{isLoggedIn && (
+						{session && (
 							<Link
 								href='/artworks/add'
 								className={`${
@@ -226,16 +242,21 @@ const Navbar = () => {
 							</Link>
 						)}
 
-						{!isLoggedIn && (
-							<button className='flex items-center text-white bg-rose-700 hover:bg-rose hover:text-white rounded-md px-3 py-2 my-4'>
-								<FaGoogle className='fa-brands fa-google mr-2'></FaGoogle>
-								<span>Login or Register</span>
-							</button>
-						)}
-					</div>
-				</div>
-			)}
-		</nav>
-	);
+{!session &&
+              providers &&
+              Object.values(providers).map((provider, index) => (
+                <button
+                  onClick={() => signIn(provider.id)}
+                  key={index}
+                  className='flex items-center text-white bg-rose-600 hover:bg-rose-900 hover:text-white rounded-md px-3 py-2'
+                >
+                  <span>Login or Register</span>
+                </button>
+              ))}
+          </div>
+        </div>
+      )}
+    </nav>
+  );
 };
 export default Navbar;
