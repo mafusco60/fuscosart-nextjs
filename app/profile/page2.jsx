b922d0ec125f007a8984cc4cce7b76f3334c2973
Mary Anne Fusco <fuscomaryanne@gmail.com>
2024-06-01 19:30:15 -0400

    Fixed some format issues on Profile Page

--- app/profile/page2.jsx
+++ app/profile/page2.jsx
@@ -13,21 +13,21 @@ const ProfilePage = () => {
   const profileName = session?.user?.name;
   const profileEmail = session?.user?.email;
 
-  const [artworks, setArtworks] = useState([]);
+  const [properties, setProperties] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUserArtworks = async (userId) => {
+    const fetchUserProperties = async (userId) => {
       if (!userId) {
         return;
       }
 
       try {
-        const res = await fetch(`/api/artworks/user/${userId}`);
+        const res = await fetch(`/api/properties/user/${userId}`);
 
         if (res.status === 200) {
           const data = await res.json();
-          setArtworks(data);
+          setProperties(data);
         }
       } catch (error) {
         console.log(error);
@@ -36,44 +36,44 @@ const ProfilePage = () => {
       }
     };
 
-    // Fetch user artworks when session is available
+    // Fetch user properties when session is available
     if (session?.user?.id) {
-      fetchUserArtworks(session.user.id);
+      fetchUserProperties(session.user.id);
     }
   }, [session]);
 
-  const handleDeleteArtwork = async (artworkId) => {
+  const handleDeleteProperty = async (propertyId) => {
     const confirmed = window.confirm(
-      'Are you sure you want to delete this artwork?'
+      'Are you sure you want to delete this property?'
     );
 
     if (!confirmed) return;
 
     try {
-      const res = await fetch(`/api/artworks/${artworkId}`, {
+      const res = await fetch(`/api/properties/${propertyId}`, {
         method: 'DELETE',
       });
 
       if (res.status === 200) {
-        // Remove the artwork from state
-        const updatedArtworks = artworks.filter(
-          (artwork) => artwork._id !== artworkId
+        // Remove the property from state
+        const updatedProperties = properties.filter(
+          (property) => property._id !== propertyId
         );
 
-        setArtworks(updatedArtworks);
+        setProperties(updatedProperties);
 
-        toast.success('Artwork Deleted');
+        toast.success('Property Deleted');
       } else {
-        toast.error('Failed to delete artwork');
+        toast.error('Failed to delete property');
       }
     } catch (error) {
       console.log(error);
-      toast.error('Failed to delete artwork');
+      toast.error('Failed to delete property');
     }
   };
 
   return (
-    <section className='bg-rose-50'>
+    <section className='bg-blue-50'>
       <div className='container m-auto py-24'>
         <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
           <h1 className='text-3xl font-bold mb-4'>Your Profile</h1>
@@ -88,28 +88,28 @@ const ProfilePage = () => {
                   alt='User'
                 />
               </div>
-              <h2 className='text-1xl mb-4'>
+              <h2 className='text-2xl mb-4'>
                 <span className='font-bold block'>Name: </span> {profileName}
               </h2>
-              <h2 className='text-1xl'>
+              <h2 className='text-2xl'>
                 <span className='font-bold block'>Email: </span> {profileEmail}
               </h2>
             </div>
 
             <div className='md:w-3/4 md:pl-4'>
               <h2 className='text-xl font-semibold mb-4'>Your Listings</h2>
-              {!loading && artworks.length === 0 && (
-                <p>You have no artwork listings</p>
+              {!loading && properties.length === 0 && (
+                <p>You have no property listings</p>
               )}
               {loading ? (
                 <Spinner loading={loading} />
               ) : (
-                artworks.map((artwork) => (
-                  <div key={artwork._id} className='mb-10'>
-                    <Link href={`/artworks/${artwork._id}`}>
+                properties.map((property) => (
+                  <div key={property._id} className='mb-10'>
+                    <Link href={`/properties/${property._id}`}>
                       <Image
                         className='h-32 w-full rounded-md object-cover'
-                        src={artwork.images[0]}
+                        src={property.images[0]}
                         alt=''
                         width={500}
                         height={100}
@@ -117,18 +117,21 @@ const ProfilePage = () => {
                       />
                     </Link>
                     <div className='mt-2'>
-                      <p className='text-lg font-semibold'>{artwork.name}</p>
-                      <p className='text-sm text-gray-500'>{artwork.type}</p>
+                      <p className='text-lg font-semibold'>{property.name}</p>
+                      <p className='text-gray-600'>
+                        Address: {property.location.street}{' '}
+                        {property.location.city} {property.location.state}
+                      </p>
                     </div>
                     <div className='mt-2'>
                       <Link
-                        href={`/artworks/${artwork._id}/edit`}
-                        className='bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-rose-600'
+                        href={`/properties/${property._id}/edit`}
+                        className='bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600'
                       >
                         Edit
                       </Link>
                       <button
-                        onClick={() => handleDeleteArtwork(artwork._id)}
+                        onClick={() => handleDeleteProperty(property._id)}
                         className='bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600'
                         type='button'
                       >
