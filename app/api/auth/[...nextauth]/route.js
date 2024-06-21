f3a13b8ff72bc1f6fb8df7f171307771e9133104
Mary Anne Fusco <fuscomaryanne@gmail.com>
2024-06-21 18:29:24 -0400

    more dynamic export

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
-import { authOptions } from '@/utils/authOptions';
-import NextAuth from 'next-auth/next';
+import { authOptions } from "@/utils/authOptions";
+import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
+export const dynamic = "force-dynamic";
 
 export { handler as GET, handler as POST };
