a9a9e35a08dc74f0384d1b72260f99467a7ea330
Mary Anne Fusco <fuscomaryanne@gmail.com>
2024-06-23 22:20:24 -0400

    another try

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,37 +1,42 @@
-import '@/assets/styles/globals.css';
-import AuthProvider from '@/components/AuthProvider';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
-import { ToastContainer } from 'react-toastify';
-import { GlobalProvider } from '@/context/GlobalContext';
-import 'react-toastify/dist/ReactToastify.css';
-import 'photoswipe/dist/photoswipe.css';
+import "@/assets/styles/globals.css";
+import AuthProvider from "@/components/AuthProvider";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
+import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 
-
-
+export const dynamic = "auto";
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = "auto";
+export const runtime = "nodejs";
+export const preferredRegion = "auto";
+export const maxDuration = 5;
 
 export const metadata = {
-	title: "Fusco's Art Gallery",
-	description: 'Find Your Favorite Artwork',
-	keywords:
-		'art, gallery, artwork, painting, drawings, prints, posters, canvas, wall art, home decor, interior design, art lovers, art collectors, art enthusiasts, art buyers, art sellers, art dealers, art investors, art critics, art historians, art students, art teachers, art professors, art curators, art directors, art consultants, art advisors, art appraisers, art conservators, art restorers',
+  title: "Fusco's Art Gallery",
+  description: "Find Your Favorite Artwork",
+  keywords:
+    "art, gallery, artwork, painting, drawings, prints, posters, canvas, wall art, home decor, interior design, art lovers, art collectors, art enthusiasts, art buyers, art sellers, art dealers, art investors, art critics, art historians, art students, art teachers, art professors, art curators, art directors, art consultants, art advisors, art appraisers, art conservators, art restorers",
 };
 
 const MainLayout = ({ children }) => {
-	return (
-		<GlobalProvider>
-			<AuthProvider>
-			<html lang='en'>
-				<body>
-					<Navbar />
-					<main>{children}</main>
-					<Footer />
-					<ToastContainer />
-				</body>
-			</html>
-			</AuthProvider>
-		</GlobalProvider>
-	);
+  return (
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
+  );
 };
 
 export default MainLayout;
